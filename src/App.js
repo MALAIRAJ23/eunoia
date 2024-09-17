075f0d38e4da0f2981ce0e624450df91e58cf68c
P-K-PRAJHAN <pkprajhan@gmail.com>
2024-09-17 14:40:11 +0530

    first push

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import logo1 from "./title.jpg";
 import logo2 from "./icon.jpg";
-import loginImage from "./getstarted.jpg"; 
-import Login from './login'; // Import the Login component
+import loginImage from "./getstarted.jpg";
+import Login from "./login"; // Import the Login component
 import "./App.css";
 
 function App() {
@@ -21,14 +21,17 @@ function App() {
         <div className="Home">
           <p>This is a page to redeem/energise your mind and soul</p>
         </div>
-        
+
         <div className="Img">
           <img src={logo1} className="App-logo1" alt="logo" />
           <img src={logo2} className="App-logo2" alt="logo" />
         </div>
-        
+
         <div className="About">
-          <p>Discover Personalized mental health insights and track your well being effortlessly</p>
+          <p>
+            Discover Personalized mental health insights and track your well
+            being effortlessly
+          </p>
         </div>
 
         {/* Button as an image */}
@@ -40,7 +43,7 @@ function App() {
 
         <div className="ftrdiv">
           <footer className="ftr">
-            <p className="ftrp">@EUNOIA - Inner Harmony</p>
+            <p className="ftrp">@EUNOIA-Inner Harmony</p>
           </footer>
         </div>
       </div>
