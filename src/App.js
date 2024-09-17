b748a7c3c8ef29a3835a55f6a8ad3894a9009b98
MALAIRAJ23 <iammalairaj@gmail.com>
2024-09-17 21:18:01 +0530

    first push

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
-import logo1 from "./title.jpg";
-import logo2 from "./icon.jpg";
-import loginImage from "./getstarted.jpg";
-import Login from "./login"; // Import the Login component
+import React from 'react';
+import logo1 from "./icon.jpg";
+import logo2 from "./title.jpg";
+import loginImage from "./getstarted.jpg"; 
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Login from './login.js'; // Ensure this is a default export from login.js
 import "./App.css";
 
 function App() {
@@ -11,46 +11,58 @@ function App() {
     <Router>
       <div className="MainPage">
         <nav className="Main">
-          <a href="#Home">Home</a>
-          <a href="#About">About</a>
-          <a href="#Engage">Engage</a>
-          <a href="#Elements">Elements</a>
-          <a href="#More">More</a>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/engage">Engage</Link>
+          <Link to="/elements">Elements</Link>
+          <Link to="/more">More</Link>
         </nav>
 
-        <div className="Home">
-          <p>This is a page to redeem/energise your mind and soul</p>
-        </div>
+        <Routes>
+          {/* Home Route */}
+          <Route path="/" element={
+            <div className="Home">
+              <p>This is a page to redeem/energize your mind and soul</p>
 
-        <div className="Img">
-          <img src={logo1} className="App-logo1" alt="logo" />
-          <img src={logo2} className="App-logo2" alt="logo" />
-        </div>
+              <div className="Img">
+                <img src={logo1} className="App-logo1" alt="Main logo" />
+                <img src={logo2} className="App-logo2" alt="Secondary logo" />
+              </div>
 
-        <div className="About">
-          <p>
-            Discover Personalized mental health insights and track your well
-            being effortlessly
-          </p>
-        </div>
+              <div className="About">
+                <p>Discover personalized mental health insights and track your well-being effortlessly</p>
+              </div>
 
-        {/* Button as an image */}
-        <div className="ButtonContainer">
-          <Link to="/login">
-            <img src={loginImage} alt="Get Started" className="ActionButton" />
-          </Link>
-        </div>
+              <div className="ButtonContainer">
+                <Link to="/login">
+                  <img src={loginImage} alt="Get Started" className="ActionButton" />
+                </Link>
+              </div>
+            </div>
+          }/>
+
+          {/* Login Route */}
+          <Route path="/login" element={<Login />} />
+          
+          {/* About Route */}
+          <Route path="/about" element={<div>About Page</div>} />
+
+          {/* Engage Route */}
+          <Route path="/engage" element={<div>Engage Page</div>} />
+
+          {/* Elements Route */}
+          <Route path="/elements" element={<div>Elements Page</div>} />
+
+          {/* More Route */}
+          <Route path="/more" element={<div>More Page</div>} />
+        </Routes>
 
         <div className="ftrdiv">
           <footer className="ftr">
-            <p className="ftrp">@EUNOIA-Inner Harmony</p>
+            <p className="ftrp">@EUNOIA - Inner Harmony</p>
           </footer>
         </div>
       </div>
-
-      <Routes>
-        <Route path="/login" element={<Login />} />
-      </Routes>
     </Router>
   );
 }
